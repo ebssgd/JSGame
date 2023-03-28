5a85730c1fb6dcc07027722a7258b488dc8cb8d2
ebssgd <iamebssgd@yahoo.com>
2023-03-28 17:53:19 -0500

    Added basic structure

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+
+    class Player {
+        constructor(game){
+            this.game = game;
+        }
+        draw(context){
+            context.beginPath();
+            context.arc();
+        }
+    }
+
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.player = new Player(this);
+        }
+    }
+
+    const game = new Game(canvas);
+    console.log(game);
+
+    function animate(){
+
+    }
+});
