5781fac11e3f75f49bd789f5d59eac259964e24c
ebssgd <iamebssgd@yahoo.com>
2023-03-28 19:43:42 -0500

    Player location and mouse controls

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ window.addEventListener('load', function(){
     canvas.width = 1280;
     canvas.height = 720;
 
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
     class Player {
         constructor(game){
             this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 50;
         }
         draw(context){
             context.beginPath();
-            context.arc();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
         }
     }
 
@@ -20,10 +32,40 @@ window.addEventListener('load', function(){
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+            //event listeners
+            window.addEventListener('mousedown', e => {
+                this.mouse.x = e.offsetX;
+                this. mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+            });
+            window.addEventListener('mouseup', e => {
+                this.mouse.x = e.offsetX;
+                this. mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+            window.addEventListener('mousemove', e => {
+                this.mouse.x = e.offsetX;
+                this. mouse.y = e.offsetY;
+                console.log(this.mouse.x);
+                //this.mouse.pressed = false;
+            });
+        }
+
+        
+
+        render(context){
+            this.player.draw(context);
         }
     }
 
     const game = new Game(canvas);
+    game.render(ctx);
     console.log(game);
 
     function animate(){
